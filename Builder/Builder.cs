325c1ec74bc910212063c1e05cfccc2caf099255
Mika Kruckenberg <mika.kruckenberg@thomas-philipps.de>
2025-07-18 07:35:58 +0200

    add Builder pattern + example

---
+++ Builder/Builder.cs
@@ -0,0 +1,63 @@
+﻿namespace DesignPatterns.Builder;
+
+/// <summary>
+/// Applicability, Use the builder pattern when:
+/// <para>
+/// - the algorithm for creating a complex object should be independent of the parts and their composition.
+/// </para>
+/// <para>
+/// the construction process should allow different representations for the object that is constructed.
+/// </para>
+/// </summary>
+public interface IBuilder
+{
+    void BuildPartA();
+    void BuildPartB();
+    Product GetResult();
+}
+
+public class ConcreteBuilder : IBuilder
+{
+    private Product _product = new();
+
+    public void BuildPartA()
+    {
+        _product.Add("PartA");
+    }
+
+    public void BuildPartB()
+    {
+        _product.Add("PartB");
+    }
+
+    public Product GetResult()
+    {
+        Product result = _product;
+        _product = new Product();
+        return result;
+    }
+}
+
+public class Product
+{
+    private readonly List<string> _parts = [];
+
+    public void Add(string part)
+    {
+        _parts.Add(part);
+    }
+
+    public override string ToString()
+    {
+        return $"Product parts: {string.Join(", ", _parts)}";
+    }
+}
+
+public class Director(IBuilder builder)
+{
+    public void Construct()
+    {
+        builder.BuildPartA();
+        builder.BuildPartB();
+    }
+}
---
+++ Builder/Example.cs
@@ -0,0 +1,26 @@
+﻿namespace DesignPatterns.Builder;
+
+public static class Example
+{
+    public static void RunWithBuilder()
+    {
+        ConcreteBuilder builder = new();
+        Director director = new(builder);
+
+        director.Construct();
+        Product product = builder.GetResult();
+
+        Console.WriteLine(product);
+    }
+
+    public static void RunWithoutDirector()
+    {
+        ConcreteBuilder builder = new();
+        builder.BuildPartA();
+        builder.BuildPartB();
+
+        Product product = builder.GetResult();
+
+        Console.WriteLine(product);
+    }
+}
