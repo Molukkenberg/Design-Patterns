b0ca72d9677b1aeb6d61812c229e50e4329d9bdc
Mika Kruckenberg <mika.kruckenberg@thomas-philipps.de>
2025-07-28 08:53:40 +0200

    add Prototype Pattern add Prototype example add Prototype diagramm

---
+++ Prototype/Prototype.cs
@@ -0,0 +1,42 @@
+﻿namespace DesignPatterns.Prototype;
+
+/// <summary>
+/// Consequences:
+/// <para>
+/// - Product supplementation and removal at runtime
+/// </para>
+/// <para>
+/// - specification of new objects by value variation
+/// </para>
+/// <para>
+/// - specification of new objcets by structural variation
+/// </para>
+/// <para>
+/// - reduced sub class creation
+/// </para>
+/// <para>
+/// - dynamic application configuration by classes
+/// </para>
+/// </summary>
+public interface IPrototype
+{
+    IProduct Clone();
+}
+
+public interface IProduct
+{
+    string Name { get; set; }
+}
+
+public class Product : IProduct, IPrototype
+{
+    public string Name { get; set; } = default!;
+
+    public IProduct Clone()
+    {
+        return new Product
+        {
+            Name = Name,
+        };
+    }
+}
---
+++ Prototype/PrototypeExample.cs
@@ -0,0 +1,18 @@
+﻿namespace DesignPatterns.Prototype;
+
+public class PrototypeExample
+{
+    static void Run()
+    {
+        Product product = new()
+        {
+            Name = "Original Product"
+        };
+
+        IProduct clonedProduct = product.Clone();
+        clonedProduct.Name = "Cloned Product";
+
+        Console.WriteLine($"Original Product Name: {product.Name}");
+        Console.WriteLine($"Cloned Product Name: {clonedProduct.Name}");
+    }
+}
