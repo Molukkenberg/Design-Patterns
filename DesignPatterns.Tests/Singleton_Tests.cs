ccef50c1534729fb42e1584bdd7ca2a0d18ddb11
Mika Kruckenberg <mika.kruckenberg@thomas-philipps.de>
2025-07-31 09:35:49 +0200

    edit project structure

---
+++ DesignPatterns.Tests/Singleton_Tests.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPatterns.Tests;
+
+using DesignPatterns.Singleton;
+
+[TestClass]
+public sealed class Singleton_Tests
+{
+    [TestMethod]
+    public void AreInstancesTheSame()
+    {
+        Singleton instance = Singleton.Instance;
+        Singleton anotherInstance = Singleton.Instance;
+
+        Assert.IsNotNull(instance);
+        Assert.AreSame(instance, anotherInstance, "Instances should be the same.");
+    }
+}
