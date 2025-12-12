507ef2a24e5edef40e006a0440d3296f6a260631
Mika Kruckenberg <Mika.kruckenberg@gmx.de>
2025-12-12 22:44:05 +0100

    move iterator example out of iterator.cs

--- DesignPatterns/Behavioral Patterns/Iterator/Iterator.cs
+++ DesignPatterns/Behavioral Patterns/Iterator/Iterator.cs
@@ -131,29 +131,3 @@ public class ConcreteIterator<T>(ConcreteAggregate<T> aggregate) : IIterator<T>
         return aggregate.GetItem(_currentIndex);
     }
 }
-
-public  class IteratorExample()
-{
-    public static void Run()
-    {
-        ConcreteAggregate<string> aggregate = new();
-
-        aggregate.AddItem("Item 1");
-        aggregate.AddItem("Item 2");
-        aggregate.AddItem("Item 3");
-
-        var iterator = aggregate.CreateIterator();
-        Console.WriteLine("Iterating forward:");
-
-        for (var item = iterator.First(); !iterator.IsDone(); item = iterator.Next())
-        {
-            Console.WriteLine(item);
-        }
-
-        Console.WriteLine("Iterating backward:");
-        for (var item = iterator.Last(); !iterator.IsDone(); item = iterator.Previous())
-        {
-            Console.WriteLine(item);
-        }
-    }
-}
---
+++ DesignPatterns/Behavioral Patterns/Iterator/IteratorExample.cs
@@ -0,0 +1,27 @@
+﻿namespace DesignPatterns.Behavioral_Patterns.Iterator;
+
+public class IteratorExample()
+{
+    public static void Run()
+    {
+        ConcreteAggregate<string> aggregate = new();
+
+        aggregate.AddItem("Item 1");
+        aggregate.AddItem("Item 2");
+        aggregate.AddItem("Item 3");
+
+        var iterator = aggregate.CreateIterator();
+        Console.WriteLine("Iterating forward:");
+
+        for (var item = iterator.First(); !iterator.IsDone(); item = iterator.Next())
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.WriteLine("Iterating backward:");
+        for (var item = iterator.Last(); !iterator.IsDone(); item = iterator.Previous())
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
