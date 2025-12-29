675d4ee33c9093f6249338cc9a8c1ad2499f07a7
Mika Kruckenberg <mika.kruckenberg@thomas-philipps.de>
2025-12-29 09:44:43 +0100

    add mediator pattern add mediator example add mediator diagram

---
+++ DesignPatterns/Behavioral Patterns/Mediator/Mediator.cs
@@ -0,0 +1,96 @@
+﻿namespace DesignPatterns.Behavioral_Patterns.Mediator;
+
+/// <summary>
+/// Purpose: Define an object that encapsulates how a set of objects interact.
+///          Mediator promotes loose coupling by keeping objects from referring to each other explicitly,
+///          
+/// Applicability: Use the Mediator pattern when
+/// - Communication between a set of objects is complex and involves many interconnections.
+/// - Reusing an object is difficult because it refers to and communicates with many other objects.
+/// - Changeability of functionality, distributed between multiple classes, should be provided
+///
+/// Participants:
+/// - Mediator: Declares an interface for communicating with Colleague objects.
+/// - ConcreteMediator: Implements cooperative behavior by coordinating Colleague objects.
+/// - Colleague classes: Each Colleague class knows its Mediator object. 
+///                      Each Colleague communicates with its Mediator 
+///                      whenever it would have otherwise communicated with another Colleague.
+/// </summary>
+public interface IMediator
+{
+    void Notify(string message, IColleague sender);
+    void RegisterColleague(IColleague colleague);
+}
+
+public interface IColleague
+{
+    IMediator Mediator { get; init; }
+
+    void Send(string message);
+
+    void Receive(string message);
+}
+
+public class ConcreteMediator : IMediator
+{
+    public ConcreteColleagueA? ColleagueA { get; private set; }
+    public ConcreteColleagueB? ColleagueB { get; private set; }
+
+    public void RegisterColleague(IColleague colleague)
+    {
+        if(colleague is ConcreteColleagueA colleagueA)
+        {
+            ColleagueA = colleagueA;
+        }
+
+        if(colleague is ConcreteColleagueB colleagueB)
+        {
+            ColleagueB = colleagueB;
+        }
+    }
+
+    public void Notify(string message, IColleague sender)
+    {
+        if(sender is ConcreteColleagueA)
+        {
+            Console.WriteLine("Mediator notifiying Colleague B");
+            ColleagueB?.Receive(message);
+        }
+
+        if(sender is ConcreteColleagueB)
+        {
+            Console.WriteLine("Mediator notifiying Colleague A");
+            ColleagueA?.Receive(message);
+        }
+    }
+}
+
+public class ConcreteColleagueA(IMediator mediator) : IColleague
+{
+    public IMediator Mediator { get; init; } = mediator;
+
+    public void Send(string message)
+    {
+        Mediator.Notify(message, this);
+    }
+
+    public void Receive(string message)
+    {
+        Console.WriteLine($"Colleague A received: {message}");
+    }
+}
+
+public class ConcreteColleagueB(IMediator mediator) : IColleague
+{
+    public IMediator Mediator { get; init; } = mediator;
+
+    public void Send(string message)
+    {
+        Mediator.Notify(message, this);
+    }
+
+    public void Receive(string message)
+    {
+        Console.WriteLine($"Colleague B received: {message}");
+    }
+}
---
+++ DesignPatterns/Behavioral Patterns/Mediator/MediatorExample.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPatterns.Behavioral_Patterns.Mediator;
+
+public static class MediatorExample
+{
+    public static void Run()
+    {
+        ConcreteMediator mediator = new();
+        ConcreteColleagueA colleagueA = new(mediator);
+        ConcreteColleagueB colleagueB = new(mediator);
+
+        mediator.RegisterColleague(colleagueA);
+        mediator.RegisterColleague(colleagueB);
+
+        colleagueA.Send("Hello from A");
+        colleagueB.Send("Hello from B");
+    }
+}
